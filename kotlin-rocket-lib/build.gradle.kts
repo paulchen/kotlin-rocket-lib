8cbe7c26d60eb809276b382367e065c111cff38c
Paul Staroch <paulchen@rueckgr.at>
2021-12-19 22:52:13 +0100

    update log4j to 2.17.0 (CVE-2021-45105)

--- kotlin-rocket-lib/build.gradle.kts
+++ kotlin-rocket-lib/build.gradle.kts
@@ -1,6 +1,6 @@
 import java.io.ByteArrayOutputStream
 
-val log4jVersion = "2.16.0"
+val log4jVersion = "2.17.0"
 val ktorVersion = "1.6.7"
 val reflectionsVersion = "0.10.2"
 var commonsCodecVersion = "1.15"
