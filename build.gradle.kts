fcddecf3caed912781a7f51f6ace8a936a3ad885
Paul Staroch <paulchen@rueckgr.at>
2023-07-04 23:30:18 +0200

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ import org.owasp.dependencycheck.reporting.ReportGenerator
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val log4jVersion = "2.20.0"
-val ktorVersion = "2.3.1"
+val ktorVersion = "2.3.2"
 val reflectionsVersion = "0.10.2"
-val commonsCodecVersion = "1.15"
+val commonsCodecVersion = "1.16.0"
 val jacksonVersion = "2.15.2"
 
 group = "at.rueckgr.kotlin.rocketbot"
@@ -18,11 +18,11 @@ plugins {
     kotlin("plugin.serialization") version "1.8.22"
     `java-library`
     `maven-publish`
-    id("com.github.ben-manes.versions") version "0.46.0"
+    id("com.github.ben-manes.versions") version "0.47.0"
     id("app.cash.licensee") version "1.7.0"
     id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
     id("signing")
-    id("org.owasp.dependencycheck") version "8.2.1"
+    id("org.owasp.dependencycheck") version "8.3.1"
 }
 
 tasks.named<DependencyUpdatesTask>("dependencyUpdates").configure {
